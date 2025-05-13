2a846a4911d65c3b5844db2c02115145b2fa3e22
afala2020 <71696868+afala2020@users.noreply.github.com>
2025-05-13 15:08:45 +0800

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -9,7 +9,7 @@ RESET="\033[0m"
 # 检测是否具有 root 权限
 [[ $EUID -ne 0 ]] && echo -e "${RED}错误: 请先运行 sudo -i 获取 root 权限后再执行此脚本${RESET}" && exit 1
 
-# 解锁服务函数
+# 解锁docker服务
 unlock_services() {
     echo -e "${YELLOW}[4/5] 正在解除 SSH 和 Docker 服务的锁定，启用密码访问...${RESET}"
     systemctl unmask ssh containerd docker.socket docker
@@ -18,7 +18,7 @@ unlock_services() {
     systemctl start ssh containerd docker.socket docker &>/dev/null
 }
 
-# SSH 配置函数
+# SSH 登录配置
 configure_ssh() {
   echo -e "${YELLOW}[2/5] 正在终止现有的 SSH 进程...${RESET}"
   lsof -i:22 | awk '/IPv4/{print $2}' | xargs kill -9 2>/dev/null || true
