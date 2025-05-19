2822338ee33c38be1483b17854846b4bf363f7b9
afala2020 <71696868+afala2020@users.noreply.github.com>
2025-05-19 22:40:53 +0800

    Update yilai.sh

--- yilai.sh
+++ yilai.sh
@@ -20,5 +20,13 @@ yes "yes" | sudo apt install -y ack antlr3 asciidoc autoconf automake autopoint 
   python3-pyelftools qemu-utils re2c rsync scons squashfs-tools subversion swig texinfo uglifyjs \
   upx-ucl unzip vim wget xmlto xxd zlib1g-dev zstd
 
+sudo apt install -y libbz2-dev expat libexpat-dev g++ gcc-multilib g++-multilib file
+
+sudo apt-get install -y build-essential flex bison cmake g++ gawk gcc-multilib g++-multilib gettext git gnutls-dev \
+  libfuse-dev libncurses5-dev libssl-dev python3 python3-pip python3-ply python3-pyelftools rsync unzip zlib1g-dev \
+  file wget subversion patch upx-ucl autoconf automake curl asciidoc binutils bzip2 lib32gcc-s1 libc6-dev-i386 \
+  uglifyjs msmtp texinfo libreadline-dev libglib2.0-dev xmlto libelf-dev libtool autopoint antlr3 gperf ccache \
+  swig coreutils haveged scons libpython3-dev rename qemu-utils jq genisoimage
+
 sleep 2
-echo "所有依赖包都已安装完成！!"
+echo "-----所有依赖包都已安装完成！-----"
